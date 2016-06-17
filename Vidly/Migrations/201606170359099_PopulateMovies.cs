3fd336b71e9a59b483065c8ca3baeaaa3c4be857
Bruno de Andrade <bruno.andrade23@gmail.com>
2016-06-17 01:23:23 -0300

    Created database and migrations to seed and add columns to tables

---
+++ Vidly/Migrations/201606170359099_PopulateMovies.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMovies : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Hangover', 1)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Die Hard', 2)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('The Terminator', 2)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Toy Story', 3)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Titanic', 4)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
